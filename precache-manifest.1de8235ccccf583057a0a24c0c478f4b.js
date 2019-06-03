199c9ceac8a289ec250d0fc8486712e2be2a5480
Praveen <praveen.kumar@tothenew.com>
2019-06-03 10:07:51 +0530

    Updates

---
+++ precache-manifest.1de8235ccccf583057a0a24c0c478f4b.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d8478353a5d84eb4cb9b60bfc67e0dd",
+    "url": "/WasteLess/index.html"
+  },
+  {
+    "revision": "7b2154f08a9dc48f0b24",
+    "url": "/WasteLess/static/css/2.95913535.chunk.css"
+  },
+  {
+    "revision": "b835dcd742436815961d",
+    "url": "/WasteLess/static/css/main.06199ca4.chunk.css"
+  },
+  {
+    "revision": "7b2154f08a9dc48f0b24",
+    "url": "/WasteLess/static/js/2.23d9a27b.chunk.js"
+  },
+  {
+    "revision": "b835dcd742436815961d",
+    "url": "/WasteLess/static/js/main.c9b2e341.chunk.js"
+  },
+  {
+    "revision": "b3a0d248457881802607",
+    "url": "/WasteLess/static/js/runtime~main.8de17462.js"
+  },
+  {
+    "revision": "6040a592f1547a451c85cb74dc0b3dbe",
+    "url": "/WasteLess/static/media/about.6040a592.svg"
+  },
+  {
+    "revision": "79447a38af85a74d9ce181f121b83832",
+    "url": "/WasteLess/static/media/carrer.79447a38.svg"
+  },
+  {
+    "revision": "4fb0451c6510f9be93640e141924ff0a",
+    "url": "/WasteLess/static/media/check.4fb0451c.svg"
+  },
+  {
+    "revision": "cb6772c8e05a2766abf8316498f8bc5f",
+    "url": "/WasteLess/static/media/color.cb6772c8.svg"
+  },
+  {
+    "revision": "2191d5b380aeb66c6a25c67bf19cf14a",
+    "url": "/WasteLess/static/media/development.2191d5b3.svg"
+  },
+  {
+    "revision": "482b6b89683192eeaa70edf6e87ce303",
+    "url": "/WasteLess/static/media/developmentProcess.482b6b89.svg"
+  },
+  {
+    "revision": "4c27adbebe3edb73a8f752646f24484d",
+    "url": "/WasteLess/static/media/dynamic_pricing.4c27adbe.svg"
+  },
+  {
+    "revision": "b95cf1b7099ec42142fe82c3772a2dc0",
+    "url": "/WasteLess/static/media/environment.b95cf1b7.svg"
+  },
+  {
+    "revision": "4085e1c3806a55309efb61b2d24172db",
+    "url": "/WasteLess/static/media/esl-installation-copy-2-min.4085e1c3.jpg"
+  },
+  {
+    "revision": "aa39d300f79c105dc82b11679946f907",
+    "url": "/WasteLess/static/media/food_waste_active.aa39d300.svg"
+  },
+  {
+    "revision": "45fac06aaaa87f3dd2f6de4eaebf0884",
+    "url": "/WasteLess/static/media/increasing_revenue_active.45fac06a.svg"
+  },
+  {
+    "revision": "6f052b08d867b0d256faf3cba4b9107a",
+    "url": "/WasteLess/static/media/installation.6f052b08.svg"
+  },
+  {
+    "revision": "a6249ba3a67c71e4f59089def400dda4",
+    "url": "/WasteLess/static/media/introduction-min.a6249ba3.jpg"
+  },
+  {
+    "revision": "36fc4ceb64e2809c3f131fb001295d50",
+    "url": "/WasteLess/static/media/introduction.36fc4ceb.svg"
+  },
+  {
+    "revision": "78200a1550b65e4a1a5bf60161b12d23",
+    "url": "/WasteLess/static/media/marketing_photo.78200a15.jpg"
+  },
+  {
+    "revision": "3abc3df0b266792f29f38642e8d7dc69",
+    "url": "/WasteLess/static/media/planning_process.3abc3df0.svg"
+  },
+  {
+    "revision": "8d6427e7348651327afd435130e0b5db",
+    "url": "/WasteLess/static/media/problem.8d6427e7.svg"
+  },
+  {
+    "revision": "720a74b4a22ead17a24e453066b3a521",
+    "url": "/WasteLess/static/media/side_arrow.720a74b4.svg"
+  },
+  {
+    "revision": "ffdab9228de329c27300e2c0e1b59503",
+    "url": "/WasteLess/static/media/side_arrow_left.ffdab922.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/WasteLess/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/WasteLess/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/WasteLess/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/WasteLess/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "534c33c72485566c1105ea3068a5248f",
+    "url": "/WasteLess/static/media/store.534c33c7.svg"
+  },
+  {
+    "revision": "a52778d950874c14c5c880c2e1f18ee9",
+    "url": "/WasteLess/static/media/summary.a52778d9.svg"
+  },
+  {
+    "revision": "bfe114b63256bf7b57def9befc6db59e",
+    "url": "/WasteLess/static/media/team.bfe114b6.svg"
+  }
+]);
